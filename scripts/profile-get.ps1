8ca8c0cde30a51d6a55e21cff223c30fd3bfca7e
DEEUSEW\DEDAUMAN <manuel.dausmann.w@sew-eurodrive.com>
2026-07-15 12:02:13 +0200

    chore(core): move vscode sync to integrated sync

--- scripts/profile-get.ps1
+++
@@ -1,4 +0,0 @@
-Get-Content "$env:APPDATA\Code\User\globalStorage\storage.json" -Raw |
-    ConvertFrom-Json |
-    Select-Object -ExpandProperty userDataProfiles |
-    ForEach-Object { "$($_.name) -> $($_.location)" }
